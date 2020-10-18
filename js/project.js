441e89ab32d8333a1d9c5dc8fb21770484589ee4
Ana <eaaania@students.eaaa.dk>
2020-10-18 17:07:18 +0200

    mobile version getting better

--- js/project.js
+++ js/project.js
@@ -12,3 +12,13 @@ window.addEventListener('load', function () {
      document.querySelector("#menu").classList.add("white");
      projectList.style.opacity = "1";
 })
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+     var x = document.getElementById("myLinks");
+     if (x.style.display === "block") {
+          x.style.display = "none";
+     } else {
+          x.style.display = "block";
+     }
+}
